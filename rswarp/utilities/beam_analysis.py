6f2425f21ba312639e7ab92378b65c33ab5177d6
James Gerity <jgerity@tamu.edu>
2016-06-30 20:46:12 +0000

    Added z-x plot, color, and accept a function for decorating figures

--- rswarp/utilities/beam_analysis.py
+++ rswarp/utilities/beam_analysis.py
@@ -22,34 +22,42 @@ def convertunits(particlearray):
 
     return dat
 
-
-def plotphasespace(particlearray):
+def plotphasespace(particlearray,color='b',decorator=None):
     fig = plt.figure(figsize=(12, 8))
 
-    ax0 = plt.subplot(2, 2, 1)
-    ax0.scatter(particlearray[:, 0], particlearray[:, 1])
+    ax0 = plt.subplot(2, 3, 1)
+    ax0.scatter(particlearray[:, 0], particlearray[:, 1], c=color)
     ax0.set_title("x-xp phase space")
     ax0.set_xlabel("x (m)")
     ax0.set_ylabel("xp (rad)")
 
-    ax1 = plt.subplot(2, 2, 2)
-    ax1.scatter(particlearray[:, 2], particlearray[:, 3])
+    ax1 = plt.subplot(2, 3, 2)
+    ax1.scatter(particlearray[:, 2], particlearray[:, 3], c=color)
     ax1.set_title("y-yp phase space")
     ax1.set_xlabel("y (m)")
     ax1.set_ylabel("yp (rad)")
 
-    ax2 = plt.subplot(2, 2, 3)
-    ax2.scatter(particlearray[:, 0], particlearray[:, 2])
+    ax2 = plt.subplot(2, 3, 3)
+    ax2.scatter(particlearray[:, 0], particlearray[:, 2], c=color)
     ax2.set_title("x-y distribution")
     ax2.set_xlabel("x (m)")
     ax2.set_ylabel("y (m)")
 
-    ax3 = plt.subplot(2, 2, 4)
-    ax3.scatter(particlearray[:, 4], particlearray[:, 5])
+    ax3 = plt.subplot(2, 3, 4)
+    ax3.scatter(particlearray[:, 4], particlearray[:, 5], c=color)
     ax3.set_title("z-pz phase space")
     ax3.set_xlabel("z (m)")
     ax3.set_ylabel("pz (MeV/c)")
 
-    fig.tight_layout()
-    plt.show()
+    ax4 = plt.subplot(2, 3, 5)
+    ax4.scatter(particlearray[:, 4], particlearray[:, 0], c=color)
+    ax4.set_title("z-x phase space")
+    ax4.set_xlabel("z (m)")
+    ax4.set_ylabel("x (m)")
 
+    if decorator is not None:
+        decorator(fig)
+
+    # fig.tight_layout()
+    plt.show()
+    return fig
