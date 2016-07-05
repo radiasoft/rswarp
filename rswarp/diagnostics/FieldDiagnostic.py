dcbe0338acf029e5466cd7aff92e4d89c2886732
James Gerity <jgerity@tamu.edu>
2016-07-05 23:35:02 +0000

    Reorganized the way vector data are written, some minor renaming to be more OpenPMD-friendly

--- rswarp/diagnostics/FieldDiagnostic.py
+++ rswarp/diagnostics/FieldDiagnostic.py
@@ -6,21 +6,50 @@ import matplotlib.pyplot as plt
 
 class FieldDiagnostic(object):
     """
-    Common functionality for field diagnostic classes
+        Common functionality for field diagnostic classes
+
+        Parameters:
+            solver: Must be a Multigrid3D object.
+            top: Object representing Warp's top package.
+            w3d: Object representing Warp's w3d package.
     """
-    def __init__(self, solver, top, period=None):
+    def __init__(self, solver, top, w3d, period=None):
         self.solver = solver
         self.top = top
+        self.w3d = w3d
         self.period = period
+        self.gridsize = [self.solver.nx, self.solver.ny, self.solver.nz]
+        self.mesh = [self.solver.xmesh, self.solver.ymesh, self.solver.zmesh]
+        self.geometryParameters = ''
 
+        if self.solver.solvergeom == self.w3d.XYZgeom:
+            self.dims = ['x', 'y', 'z']
+            self.geometry = 'cartesian'
+        elif self.solver.solvergeom == self.w3d.RZgeom:
+            self.dims = ['r', 't', 'z']
+            self.geometry = 'thetaMode'
+            self.geometryParameters = 'm=0'
+        else:
+            raise Exception("No handler for geometry type %i" % self.solver.geomtype)
+
+    # Is it really necesary to gather the mesh here, or can it be done only in __init__?
     def gathermesh(self):
-        self.nx = self.solver.nx
-        self.ny = self.solver.ny
-        self.nz = self.solver.nz
+        self.mesh = [self.solver.xmesh, self.solver.ymesh, self.solver.zmesh]
+        if self.solver.solvergeom not in [self.w3d.XYZgeom, self.w3d.RZgeom]:
+            raise Exception("No handler for geometry type %i" % self.solver.geomtype)
 
-        self.xmesh = self.solver.xmesh
-        self.ymesh = self.solver.ymesh
-        self.zmesh = self.solver.zmesh
+    def writeDataset(self, data, prefix, attrs={}):
+        if len(data.shape) == 3:  # Scalar data on the mesh
+            self.file[prefix] = data
+        elif len(data.shape) == 4:  # Vector data on the mesh
+            for i, v in enumerate(data):
+                self.file['%s/%s' % (prefix, self.dims[i])] = v
+                self.file['%s' % prefix].attrs['n%s' % self.dims[i]] = self.gridsize[i]
+        else:
+            raise Exception("Unknown data shape: %s" % data.shape)
+
+        for k, v in attrs.items():
+            self.file[prefix].attrs[k] = v
 
     def write(self, prefix='field'):
         if self.period and self.top.it % self.period != 0:
@@ -36,13 +65,10 @@ class FieldDiagnostic(object):
         self.file = h5.File(filename, 'w')
 
         self.gathermesh()
-        self.file.create_dataset('mesh/x', data=self.xmesh)
-        self.file.create_dataset('mesh/y', data=self.ymesh)
-        self.file.create_dataset('mesh/z', data=self.zmesh)
-
-        self.file.attrs['nx'] = self.nx
-        self.file.attrs['ny'] = self.ny
-        self.file.attrs['nz'] = self.nz
+        for i, v in enumerate(self.mesh):
+            self.file['/data/meshes/mesh/%s' % self.dims[i]] = v
+        self.file['/data/meshes/mesh'].attrs['geometry'] = self.geometry
+        self.file['/data/meshes/mesh'].attrs['geometryParameters'] = self.geometryParameters
 
         return True
 
@@ -51,25 +77,24 @@ class ElectrostaticFields(FieldDiagnostic):
     """
         Produce an HDF5 file with electric fields and potential.
         File tree:
-        /mesh
-            /x
-            /y
-            /z
-        attributes: nx, ny, nz
+        /data/meshes
+            /mesh
+                /x
+                /y
+                /z
 
-        /potential
-            /electric - [xdim,ydim,zdim]
-        /fields
-            /electric - [fieldaxis,xdim,ydim,zdim]
+            Note that the coordinates will be replaced as appropriate for different
+            solver geometries (e.g. xyz -> rtz for RZgeom).
 
-        Parameters:
-            solver: Must be a Multigrid3D object.
-            top: Object representing Warp's top package.
+            /potential
+                /electric - [xdim,ydim,zdim]
+            /fields
+                /electric - [fieldaxis,xdim,ydim,zdim]
+
     """
 
     def gatherfields(self):
         self.efield = self.solver.getselfe()
-        return self.efield
 
     def gatherpotential(self):
         self.phi = self.solver.getphi()
@@ -81,8 +106,8 @@ class ElectrostaticFields(FieldDiagnostic):
         self.gatherfields()
         self.gatherpotential()
 
-        self.file.create_dataset('fields/electric', data=self.efield)
-        self.file.create_dataset('potential/electric', data=self.phi)
+        self.writeDataset(self.efield, prefix='/data/meshes/fields/electric')
+        self.writeDataset(self.phi, prefix='/data/meshes/potential/electric')
 
         self.file.close()
 
@@ -91,25 +116,24 @@ class MagnetostaticFields(FieldDiagnostic):
     """
         Produce an HDF5 file with magnetic fields and vector potential.
         File tree:
-        /mesh
-            /x
-            /y
-            /z
-        attributes: nx, ny, nz
+        /data/meshes/
+            /mesh
+                /x
+                /y
+                /z
 
-        /potential
-            /vector - [vectoraxis,xdim,ydim,zdim]
-        /fields
-            /magnetic - [fieldaxis,xdim,ydim,zdim]
+            Note that the coordinates will be replaced as appropriate for different
+            solver geometries (e.g. xyz -> rtz for RZgeom).
 
-        Parameters:
-            solver: Must be a MagnetostaticMG object.
-            top: Object representing Warp's top package.
+            /potential
+                /vector - [vectoraxis,xdim,ydim,zdim]
+            /fields
+                /magnetic - [fieldaxis,xdim,ydim,zdim]
+
     """
 
     def gatherfields(self):
         self.bfield = self.solver.getb()
-        return self.bfield
 
     def gathervectorpotential(self):
         self.a = self.solver.geta()
@@ -117,11 +141,11 @@ class MagnetostaticFields(FieldDiagnostic):
     def write(self, prefix='diags/fields/magnetic/bfield'):
         if not super(MagnetostaticFields, self).write(prefix):
             return False
-            
+
         self.gatherfields()
         self.gathervectorpotential()
 
-        self.file.create_dataset('fields/magnetic', data=self.bfield)
-        self.file.create_dataset('potential/vector', data=self.a)
+        self.writeDataset(self.bfield, prefix='/data/meshes/fields/magnetic')
+        self.writeDataset(self.a, prefix='/data/meshes/potential/vector')
 
         self.file.close()
