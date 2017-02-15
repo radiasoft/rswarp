47ea7728fd8878b7f08030cafe229a1a2d68aa38
chall <chall@radiasoft.net>
2017-02-14 22:51:21 -0700

    First commit of ConductorDiagnostics.

---
+++ rswarp/diagnostics/ConductorDiagnostics.py
@@ -0,0 +1,30 @@
+from scipy.constants import e
+import numpy as np
+
+
+def analyze_scraped_particles(top, particles, solver):
+    """
+    Returns dictionary with conductor id's as keys. Each entry
+    contains an array of form [step, particles deposited on step]
+    (Assumes only one species present in simulation)
+    Args:
+        top: Warp's top object.
+        particles: Species object for particles.
+        solver: Electrostatic solver object.
+
+    Returns:
+        [step, particles deposited on step]
+    """
+    cond_ids = []
+    cond_objs = []
+    lost = {}
+    for cond in solver.conductordatalist:
+        cond_objs.append(cond[0])
+        cond_ids.append(cond[0].condid)
+
+    for i, ids in enumerate(cond_ids):
+        lost[ids] = np.copy(cond_objs[i].lostparticles_data[:, 0:2])
+        lost[ids][:, 0] = np.ndarray.astype(np.round(lost[ids][:, 0] / top.dt), 'int')
+        lost[ids][:, 1] = np.round(-1. * lost[ids][:, 1] / particles.sw / e)
+
+    return lost
