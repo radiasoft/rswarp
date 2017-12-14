Fixed area at 1 cm**2 for resistance calculation Looking for backwards travelling particles run_time is actually a time now, not number of steps

rswarp/run_files/efficiency.py
@@ -57,14 +57,14 @@ def rd_current(phi, T):
     return A * T ** 2 * np.exp(-phi / (k_ev * T)) * 0.01**2
 
 
-def calculate_resistance(T, L_wire, rho, alpha, A_em, T_ref=300., **kwargs):
+def calculate_resistance(T, L_wire, rho, alpha, T_ref=300., **kwargs):
     """
     Temperature dependent resistance model.
     Area will be the area of the collector/emitter. Provides reasonable scaling since the wire gauge will vary
     depending on current which in turn is a function of unit area of the system.
     Args:
         T: Temperature of wire (K)
-        A: Area (cm**2)
+        A: Area (cm**2) [Fixed at 1 cm**2 for now]
         L_wire: Length of wire (m)
         rho: Resistivity (Ohms/m)
         alpha: Reference resistance coefficient
@@ -74,7 +74,7 @@ def calculate_resistance(T, L_wire, rho, alpha, A_em, T_ref=300., **kwargs):
         Resistance (Ohms)
     """
 
-    L_wire, rho, alpha, A = L_wire[0], rho[0], alpha[0], A_em[0]
+    L_wire, rho, alpha, A = L_wire[0], rho[0], alpha[0], 1.
 
     delta_T = T - T_ref
 
@@ -182,7 +182,7 @@ def calculate_efficiency(A_em, R_ew, P_em, J_em, phi_em, T_em,
     P_load = J_load * V_load
 
     # P_gate
-    P_gate = J_grid * V_grid
+    P_gate = J_grid * A_em * (1. - t) * V_grid
 
     eta = (P_load - P_gate) / (P_ec + P_r + P_ew)
 

rswarp/run_files/gridded_tec_3d.py
@@ -420,7 +420,7 @@ def main(x_struts, y_struts, V_grid, grid_height, strut_width, strut_height,
                                 ZCross.getvy(js=measurement_beam.js),
                                 ZCross.getvz(js=measurement_beam.js)]).transpose())
             ZCross.clear()  # Clear ZcrossingParticles memory
-
+        print "Backwards particles: {}".format(np.where(emitter_flux[-1][:, 2] < 0.)[0].shape)
         print(" Wind-down: Taking {} steps, On Step: {}, {} Particles Left".format(ss_check_interval, top.it,
                                                                                    measurement_beam.npsim[0]))
     ######################
@@ -444,7 +444,7 @@ def main(x_struts, y_struts, V_grid, grid_height, strut_width, strut_height,
     efficiency.tec_parameters['R_cw'][0] = efficiency.calculate_resistance(efficiency.tec_parameters['T_coll'][0],
                                                                         **efficiency.tec_parameters)
     # Set derived parameters from simulation
-    efficiency.tec_parameters['run_time'][0] = crossing_measurements * steps_per_crossing
+    efficiency.tec_parameters['run_time'][0] = crossing_measurements * steps_per_crossing * dt
     efficiency.tec_parameters['J_em'][0] = (emitter_flux.shape[0] - measured_charge[scraper_dictionary['source']]) \
                                         * measurement_beam.sw / \
                                         efficiency.tec_parameters['run_time'][0] / efficiency.tec_parameters['A_em'][0]