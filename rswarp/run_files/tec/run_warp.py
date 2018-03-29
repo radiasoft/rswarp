f6ba8818747f2ef365ea950af2d14f0f72589722
ncook882 <ncook@radiasoft.net>
2018-03-29 17:18:58 +0000

    Updated run_files/tec version of run_warp to reflect changes to tests/run_warp

--- rswarp/run_files/tec/run_warp.py
+++ rswarp/run_files/tec/run_warp.py
@@ -1,42 +1,59 @@
+"""
+Setting up test for efficiency calculation.
+Parameters compared against Voesch et al Energy Technol 2017, 5, 1-11
+Expected efficiency ~58% with no grid losses
+"""
+
 import sys
 # Path for use on jupyter.radiasoft
-sys.path.append('/home/vagrant/jupyter/repos/rswarp/rswarp/run_files/')
-
+sys.path.append('/home/vagrant/jupyter/repos/rswarp/rswarp/run_files/tec/')
+# Path for use on local
+sys.path.append('/Users/chall/research/github/rswarp/rswarp/run_files/tec/')
 try:
     from gridded_tec_3d import main
 except ImportError:
     try:
-        from rswarp.run_files.gridded_tec_3d import main
+        from rswarp.run_files.tec.gridded_tec_3d import main
     except ImportError:
-        raise ImportError, "Could not find rswarp.run_files.gridded_tec_3d"
+        raise ImportError, "Could not find rswarp.run_files.tec.gridded_tec_3d"
 
 if __name__ == '__main__':
     """
-    nx: Number of struts that intercept the x-axis.
-    ny: Number of struts that intercept the y-axis
-    volts_on_grid: Voltage to place on the grid in Volts.
-    grid_height: Distance from the emitter to the grid in meters.
+    x_struts: Number of struts that intercept the x-axis.
+    y_struts: Number of struts that intercept the y-axis
+    V_grid: Voltage to place on the grid in Volts.
+    gap_distance: Distance from the emitter to the collector in meters
+    grid_height: Distance from the emitter to the grid normalized by gap distance.
     strut_width: Transverse extent of the struts in meters.
     strut_height: Longitudinal extent of the struts in meters.
-    run_id: Run id. Mainly used for parallel optimization.
-    
-    Example values: 4 8 17.7265855605 1.07243108547e-07 1.97828380067e-09 1.7538111785e-08 1
+    T_em: Temperature of emitter in Kelvin
+    phi_em: Reistivity of the emitter side wiring in ohm*cm
+    T_coll: Temperature of collector in Kelvin
+    phi_cw: Resistivity of collector side wiring in ohm*cm
+    run_id: Run id will be added to diagnostic folder name. Mainly used for parallel optimization.
     """
     args = sys.argv[1:]
 
-    nx = int(args[0])
-    ny = int(args[1])
-    volts_on_grid = float(args[2])
-    grid_height = float(args[3])
-    strut_width = float(args[4])
-    strut_height = float(args[5])
+    # Values based on Voesch et al.
+    x_struts = 1
+    y_struts = 1
+    V_grid = 15.0
+    grid_height = 0.5
+    strut_width = 2e-9
+    strut_height = 2e-9
+    rho_ew = 1.1984448e-03
+    T_em = 1414 + 273.15
+    phi_em = 2.174
+    T_coll = 50 + 273.15
+    phi_coll = 0.381
+    rho_cw = 1.1984448e-03
+    gap_distance = 1e-6
+    rho_load = 0.01648048  # matched for phi_em = 2.174, phi_coll = 0.381, and  rho_cw = rho_ew = 1.1984448e-03
+    run_id = 0
 
-    run_id = int(args[6])
+    main(x_struts, y_struts, V_grid, grid_height, strut_width, strut_height,
+         rho_ew, T_em, phi_em, T_coll, phi_coll, rho_cw, gap_distance, rho_load,
+         run_id,
+         injection_type=2, random_seed=True, install_grid=False, max_wall_time=1e9,
+         particle_diagnostic_switch=True, field_diagnostic_switch=False, lost_diagnostic_switch=False)
 
-    main(nx, ny, volts_on_grid,
-         grid_height, strut_width, strut_height, run_id,
-         random_seed=False, install_grid=False,
-         injection_type=2, cathode_temperature=1273.15,
-         particle_diagnostic_switch=True, field_diagnostic_switch=False, lost_diagnostic_flag=True)
-
-    print 0
