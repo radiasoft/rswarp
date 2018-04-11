c05f80081cdf68a7433d0ca154aede90cac2acd5
chall <chall@radiasoft.net>
2018-04-11 13:59:46 -0600

    Changed temperature bounds to emitter/collector to prevent 'negative' power flow Added new mutation operator. Changed template to include parameters for new operator

--- rswarp/utilities/deap_functions.py
+++ rswarp/utilities/deap_functions.py
@@ -10,115 +10,115 @@ Note that `random.randint` has inclusive lower and upper bounds
 tec_template = {
     'T_coll': {'type': 'float',
                'min': 50.0,
-               'max': 2000.0,
+               'max': 900.0,
                'mutation': {
                    'probability': 0.25,
-                   'mean': 0,
-                   'std': 0.5
+                   'r': 0.1,
+                   'k': 10.0
                }},
     'T_em': {'type': 'float',
-             'min': 50.0,
-             'max': 2000.0,
+             'min': 900.0,
+             'max': 1900.0,
              'mutation': {
                  'probability': 0.25,
-                 'mean': 0,
-                 'std': 0.5
+                 'r': 0.1,
+                 'k': 10.0
              }},
     'V_grid': {'type': 'float',
                'min': 0.0,
                'max': 20.0,
                'mutation': {
                    'probability': 0.25,
-                   'mean': 0,
-                   'std': 0.5
+                   'r': 0.1,
+                   'k': 10.0
                }},
     'gap_distance': {'type': 'float',
                      'min': 1e-6,
                      'max': 20e-6,
                      'mutation': {
                          'probability': 0.25,
-                         'mean': 0,
-                         'std': 0.5
+                         'r': 0.1,
+                         'k': 10.0
                      }},
     'grid_height': {'type': 'float',
                     'min': 0.05,
                     'max': 0.95,
                     'mutation': {
                         'probability': 0.25,
-                        'mean': 0,
-                        'std': 0.5
+                        'r': 0.1,
+                        'k': 10.0
                     }},
     'phi_coll': {'type': 'float',
-                 'min': 0.3,
+                 'min': 1.5,
                  'max': 4.0,
                  'mutation': {
                      'probability': 0.25,
-                     'mean': 0,
-                     'std': 0.5
+                     'r': 0.1,
+                     'k': 10.0
                  }},
     'phi_em': {'type': 'float',
-               'min': 0.3,
+               'min': 1.5,
                'max': 4.0,
                'mutation': {
                    'probability': 0.25,
-                   'mean': 0,
-                   'std': 0.5
+                   'r': 0.1,
+                   'k': 10.0
                }},
     'rho_cw': {'type': 'float',
                'min': 1e-5,
                'max': 1e-2,
                'mutation': {
                    'probability': 0.25,
-                   'mean': 0,
-                   'std': 0.5
+                   'r': 0.1,
+                   'k': 10.0
                }},
     'rho_ew': {'type': 'float',
                'min': 1e-5,
                'max': 1e-2,
                'mutation': {
                    'probability': 0.25,
-                   'mean': 0,
-                   'std': 0.5
+                   'r': 1,
+                   'k': 10.0
                }},
     'rho_load': {'type': 'float',
                  'min': 1e-4,
                  'max': 0.1,
                  'mutation': {
                      'probability': 0.25,
-                     'mean': 0,
-                     'std': 0.5
+                     'r': 0.1,
+                     'k': 10.0
                  }},
     'strut_height': {'type': 'float',
                      'min': 10e-9,
                      'max': 20e-9,
                      'mutation': {
                          'probability': 0.25,
-                         'mean': 0,
-                         'std': 0.5
+                         'r': 0.1,
+                         'k': 10.0
                      }},
     'strut_width': {'type': 'float',
                     'min': 10e-9,
                     'max': 20e-9,
                     'mutation': {
                         'probability': 0.25,
-                        'mean': 0,
-                        'std': 0.5
+                        'r': 0.1,
+                        'k': 10.0
                     }},
     'x_struts': {'type': 'int',
                  'min': 1,
                  'max': 4,
                  'mutation': {
                      'probability': 0.25,
-                     'lower': 1,
-                     'upper': 5
+                     'r': 0.5,
+                     'k': 2
                  }},
     'y_struts': {'type': 'int',
                  'min': 1,
                  'max': 4,
                  'mutation': {
                      'probability': 0.25,
-                     'lower': 1,
-                     'upper': 5
+                     'r': 0.5,
+                     'k': 2
                  }}
 }
 
@@ -159,6 +159,48 @@ def mutIntorGauss(individual, template=tec_template):
                 else:
                     sgn = +1.0
                 individual[key] = abs(individual[key] + sgn * random.gauss(m, s))
+    return individual,
+
+
+def mutBoundedExp(individual, r, k, template=tec_template):
+    """
+    Describes a variable step size bounded mutation operator.
+    Performs Var_i = Var_i + s_i * r_i * a_i. Chance for variable to mutate should be given in template.
+    s_i: random from {-1, 1}
+    r_i: r * domain_i
+
+    If the resulting Var_i is not within the variables bounds it will be set to it's closest bound.
+    From:
+    Muhlenbein, H. and Schlierkamp-Voosen, D.:
+    Predictive Models for the Breeder Genetic Algorithm: I. Continuous Parameter Optimization. Evolutionary Computation,
+    1 (1), pp. 25-49, 1993.
+    Args:
+        individual: Individual created from DEAP
+        r: Range factor, normally in [0.1, 1e-6]
+        k: mutation precision, normally in {4, 5, ... 20}
+        template: Dictionary providing template for each variables constraints and properties.
+
+    Returns:
+
+    """
+    for key in individual:
+        indpbInteger = template[key]['mutation']['probability']
+        if random.random() >= indpbInteger:
+            continue
+
+        si = random.sample([-1.0, 1.0], 1)[0]
+        ri = r * random.uniform(template[key]['min'], template[key]['max'])
+        u = random.random()
+        ai = 2**(-u * k)
+
+        individual[key] = individual[key] + si * ri * ai
+        if template[key]['type'] == int:
+            individual[key] = int(individual[key])
+        if individual[key] < template[key]['min']:
+            individual[key] = template[key]['min']
+        elif individual[key] > template[key]['max']:
+            individual[key] = template[key]['max']
+
     return individual,
 
 
