9abb2a578ca1bab229e10a54b0b5e3980fbb4144
chall <chall@radiasoft.net>
2018-04-12 14:18:38 -0600

    Changed mutation probabilities to 1/#genes Fixed range of mutation operator Fixed bug in index of crossover operator Added in start of optimizer function

--- rswarp/utilities/deap_functions.py
+++ rswarp/utilities/deap_functions.py
@@ -1,5 +1,7 @@
-import random
-
+import random, os
+from rswarp.utilities.deap_interface import JobRunner, return_efficiency, save_generation
+from deap import tools
+from deap.algorithms import varAnd
 """
 Using `random` package for random number generation. 
 This is to keep in line with the implementation in the DEAP library.
@@ -12,7 +14,7 @@ tec_template = {
                'min': 50.0,
                'max': 900.0,
                'mutation': {
-                   'probability': 0.25,
+                   'probability': 1/14.,
                    'r': 0.1,
                    'k': 10.0
                }},
@@ -20,7 +22,7 @@ tec_template = {
              'min': 900.0,
              'max': 1900.0,
              'mutation': {
-                 'probability': 0.25,
+                 'probability': 1/14.,
                  'r': 0.1,
                  'k': 10.0
              }},
@@ -28,7 +30,7 @@ tec_template = {
                'min': 0.0,
                'max': 20.0,
                'mutation': {
-                   'probability': 0.25,
+                   'probability': 1/14.,
                    'r': 0.1,
                    'k': 10.0
                }},
@@ -36,7 +38,7 @@ tec_template = {
                      'min': 1e-6,
                      'max': 20e-6,
                      'mutation': {
-                         'probability': 0.25,
+                         'probability': 1/14.,
                          'r': 0.1,
                          'k': 10.0
                      }},
@@ -44,7 +46,7 @@ tec_template = {
                     'min': 0.05,
                     'max': 0.95,
                     'mutation': {
-                        'probability': 0.25,
+                        'probability': 1/14.,
                         'r': 0.1,
                         'k': 10.0
                     }},
@@ -52,7 +54,7 @@ tec_template = {
                  'min': 1.5,
                  'max': 4.0,
                  'mutation': {
-                     'probability': 0.25,
+                     'probability': 1/14.,
                      'r': 0.1,
                      'k': 10.0
                  }},
@@ -60,7 +62,7 @@ tec_template = {
                'min': 1.5,
                'max': 4.0,
                'mutation': {
-                   'probability': 0.25,
+                   'probability': 1/14.,
                    'r': 0.1,
                    'k': 10.0
                }},
@@ -68,7 +70,7 @@ tec_template = {
                'min': 1e-5,
                'max': 1e-2,
                'mutation': {
-                   'probability': 0.25,
+                   'probability': 1/14.,
                    'r': 0.1,
                    'k': 10.0
                }},
@@ -76,7 +78,7 @@ tec_template = {
                'min': 1e-5,
                'max': 1e-2,
                'mutation': {
-                   'probability': 0.25,
+                   'probability': 1/14.,
                    'r': 1,
                    'k': 10.0
                }},
@@ -84,7 +86,7 @@ tec_template = {
                  'min': 1e-4,
                  'max': 0.1,
                  'mutation': {
-                     'probability': 0.25,
+                     'probability': 1/14.,
                      'r': 0.1,
                      'k': 10.0
                  }},
@@ -92,7 +94,7 @@ tec_template = {
                      'min': 10e-9,
                      'max': 20e-9,
                      'mutation': {
-                         'probability': 0.25,
+                         'probability': 1/14.,
                          'r': 0.1,
                          'k': 10.0
                      }},
@@ -100,7 +102,7 @@ tec_template = {
                     'min': 10e-9,
                     'max': 20e-9,
                     'mutation': {
-                        'probability': 0.25,
+                        'probability': 1/14.,
                         'r': 0.1,
                         'k': 10.0
                     }},
@@ -108,7 +110,7 @@ tec_template = {
                  'min': 1,
                  'max': 4,
                  'mutation': {
-                     'probability': 0.25,
+                     'probability': 1/14.,
                      'r': 0.5,
                      'k': 2
                  }},
@@ -116,7 +118,7 @@ tec_template = {
                  'min': 1,
                  'max': 4,
                  'mutation': {
-                     'probability': 0.25,
+                     'probability': 1/14.,
                      'r': 0.5,
                      'k': 2
                  }}
@@ -174,6 +176,7 @@ def mutBoundedExp(individual, template=tec_template):
     Muhlenbein, H. and Schlierkamp-Voosen, D.:
     Predictive Models for the Breeder Genetic Algorithm: I. Continuous Parameter Optimization. Evolutionary Computation,
     1 (1), pp. 25-49, 1993.
+    http://www.geatbx.com/docu/algindex-04.html#P659_42386
     Args:
         individual: Individual created from DEAP
         r: Range factor, normally in [0.1, 1e-6]
@@ -191,7 +194,7 @@ def mutBoundedExp(individual, template=tec_template):
             continue
 
         si = random.sample([-1.0, 1.0], 1)[0]
-        ri = r * random.uniform(template[key]['min'], template[key]['max'])
+        ri = r * (template[key]['max'] - template[key]['min'])
         u = random.random()
         ai = 2**(-u * k)
 
@@ -211,18 +214,84 @@ def cxUniform(ind1, ind2, prob=0.5, template=tec_template):
     """
     Execute a gene level cross over with probability of prob for any given gene mutation.
     """
-    k1, k2, r1, r2 = [random.uniform(0, 1) for _ in range(3)]
+    k1, k2, r1, r2 = [random.uniform(0, 1) for _ in range(4)]
     for key in ind1:
         if random.random() < prob:
             S1, S2 = (1. + r1) * (k1 * ind1[key] + (1. - k1) * ind2[key]), \
                      (1. + r2) * (k2 * ind1[key] + (1. - k2) * ind2[key])
-            if S1 < template[key]['min'] or S1 > template[key]['max']:
+            if (S1 < template[key]['min']) or (S1 > template[key]['max']):
                 S1 = S1 / (1. + r1)
-            if S2 < template[key]['min'] or S2 > template[key]['max']:
-                S2 = S2 / (1. + r1)
+            if (S2 < template[key]['min']) or (S2 > template[key]['max']):
+                S2 = S2 / (1. + r2)
             if template[key]['type'] == 'int':
                 ind1[key], ind2[key] = int(S1), int(S2)
             else:
-                ind1[key], ind2[key] = int(S1), int(S2)
+                ind1[key], ind2[key] = S1, S2
 
     return ind1, ind2
+
+
+def evaluate_efficiency(runner, remote_directories, upload_filelist, batch_files):
+    upload_status = runner.upload_file(remote_directories, upload_filelist)
+    assert upload_status == 0, "Upload failed to complete"
+
+    job_ids = runner.start_job(remote_directories, batch_files)
+    assert len(job_ids) == len(batch_files), "Failed to start all jobs"
+
+
+def eaSimpleWarp(population, toolbox, filename, cxpb, mutpb, ngen, stats=None,
+                 halloffame=None, verbose=__debug__, labels=None):
+    logbook = tools.Logbook()
+    logbook.header = ['gen', 'nevals'] + (stats.fields if stats else [])
+
+    # Evaluate the individuals with an invalid fitness
+    # TODO: Change fitness evaluation to operate on the population not on individuals
+    # TODO because all fitnesses are returned from WARP we need to pass all to the batch script simulatneously
+    # TODO and then wait.
+    # generate_fitnesses(population, toolbox)
+
+    # Find which individuals have no fitness assigned (probably all of them here)
+    fitnesses = return_efficiency(population, 'generation_0')
+    print fitnesses
+    # Assign new fitness values to the individuals
+    for ind, fit in zip(population, fitnesses):
+        ind.fitness.values = (fit,)
+    # Update HoF
+    if halloffame is not None:
+        halloffame.update(population)
+
+    save_generation(filename, population, generation=0, labels=None)
+
+    # Begin the generational process
+    for gen in range(1, ngen + 1):
+        print "Starting Generation: {}".format(gen)
+        # Select the next generation individuals
+        offspring = toolbox.select(population, len(population))
+
+        # Vary the pool of individuals
+        offspring = varAnd(offspring, toolbox, cxpb, mutpb)
+
+        # ######################## Start server setup routine ##################################################
+
+        # ######################## End server setup routine ####################################################
+        fitnesses = return_efficiency(offspring, 'generation_{}'.format(gen))
+        for ind, fit in zip(offspring, fitnesses):
+            ind.fitness.values = (fit,)
+
+        # Update the hall of fame with the generated individuals
+        if halloffame is not None:
+            halloffame.update(offspring)
+
+        # Replace the current population by the offspring
+        population[:] = offspring
+
+        # Append the current generation statistics to the logbook and save data file
+        save_generation(filename, population, generation=gen, labels=None)
+        #         record = stats.compile(population) if stats else {}
+        #         logbook.record(gen=gen, nevals=len(invalid_ind), **record)
+        #         if verbose:
+        #             print logbook.stream
+        print "Completed Generation: {}".format(gen)
+
+    # TODO switch return back to population
+    return offspring, logbook
