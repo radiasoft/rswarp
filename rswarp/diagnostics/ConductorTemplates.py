7b6f23d265f287abf8bc49119c372e36aa22c2aa
chall <chall@radiasoft.net>
2018-08-15 18:05:15 -0600

    Fixed PID indexing for single-surface objects

--- rswarp/diagnostics/ConductorTemplates.py
+++ rswarp/diagnostics/ConductorTemplates.py
@@ -8,6 +8,7 @@ class Conductor(object):
     def __init__(self, top, w3d, conductor):
         self.top = top
         self.conductor = conductor
+        self.numlost = top.npslost[0]
         self.faces = []
         self.pids = None
         self.center = None
@@ -27,21 +28,24 @@ class Conductor(object):
             self.cell_size = [w3d.dx, w3d.dy, w3d.dz]
 
     def get_pids(self):
-        scraped_particles = np.where(self.top.pidlost[:, -1] == self.conductor.condid)[0]
+        scraped_particles = np.where(self.top.pidlost[:self.numlost, -1] == self.conductor.condid)[0]
 
         return scraped_particles
 
     def _generate_face(self, resolution=100):
-        scraped_parts = np.array([self.top.xplost, self.top.yplost, self.top.zplost])[self.pids]
+        scraped_parts = np.array([self.top.xplost, self.top.yplost, self.top.zplost]).T[self.pids, :]
         for bound, axis, face in zip(self.cbounds, self.axis, self.faces):
             x, y = np.meshgrid(np.linspace(self.cbounds[(axis + 1) % 3],
                                            self.cbounds[(axis + 1) % 3 + 3], resolution),
-                               np.linspace(self.cbounds[(axis + 2) % 3] * 1e6,
+                               np.linspace(self.cbounds[(axis + 2) % 3],
                                            self.cbounds[(axis + 2) % 3 + 3], resolution))
             z = np.ones_like(x) * bound
             positions = np.vstack([x.ravel(), y.ravel()])
-            kernel = gaussian_kde(scraped_parts[face, :][:, [(axis + 1) % 3, (axis + 2) % 3]].T * 1e6)
-            s = np.reshape(kernel(positions).T, x.shape)
+            if scraped_parts.size > 1:
+                kernel = gaussian_kde(scraped_parts[face, :][:, [(axis + 1) % 3, (axis + 2) % 3]].T)
+                s = np.reshape(kernel(positions).T, x.shape)
+            else:
+                s = np.ones_like(x)
 
             yield x, y, z, s
 
@@ -59,9 +63,8 @@ class BoxPlot(Conductor):
         self.cbounds = np.hstack([mine, maxe])
 
     def get_particles(self):
-        scraped_parts = np.array([self.top.xplost, self.top.yplost, self.top.zplost])
-
-        for bound, axis, cell_size in zip(self.cbounds, self.axis, self.cell_size):
+        scraped_parts = np.array([self.top.xplost, self.top.yplost, self.top.zplost]).T[self.pids, :]
+        for bound, axis, cell_size in zip(self.cbounds, self.axis, 2 * self.cell_size):
             pof = np.where(np.abs(scraped_parts[:, axis] - bound) <= cell_size)
             self.faces.append(pof[0])
 
@@ -89,15 +92,18 @@ class YPlanePlot(Conductor):
 class ZPlanePlot(Conductor):
     def __init__(self, top, w3d, conductor):
         super(ZPlanePlot, self).__init__(top, w3d, conductor)
+        self.axis = [2, ]
         self.center = [conductor.zcent, ]
+        self.cbounds = self.domain
 
     def get_particles(self):
-        scraped_parts = np.array([self.top.xplost, self.top.yplost, self.top.zplost])[self.pids]
-
-        self.faces.append(scraped_parts)
+        # The plane has only one face so we use all particles with appropriate pid
+        self.faces.append(np.arange(self.pids.size))
 
     def generate_faces(self):
-        for axis, mesh in zip(self.axis, self._generate_face()):
+        if len(self.faces) == 0:
+            self.get_particles()
+        for mesh in self._generate_face():
             x, y, z = mesh[0], mesh[1], mesh[2]
             s = mesh[3]
             yield x, y, z, s
