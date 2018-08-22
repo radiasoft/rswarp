31fce23d61b64f2e80512a089558c0e802438b19
chall <chall@radiasoft.net>
2018-08-22 15:33:27 -0600

    Added a potentially faster interpolation method through scipy.interpolate.griddata

--- rswarp/diagnostics/ConductorTemplates.py
+++ rswarp/diagnostics/ConductorTemplates.py
@@ -1,11 +1,14 @@
 import numpy as np
 from warp.field_solvers.generateconductors import XPlane, YPlane, ZPlane, Box
 from scipy.stats import gaussian_kde
+from scipy.interpolate import griddata
 
 
 class Conductor(object):
 
-    def __init__(self, top, w3d, conductor):
+    def __init__(self, top, w3d, conductor, interpolation='kde'):
+        assert interpolation == 'kde' or interpolation == 'cubic', "Interpolation must be either 'cubic' or 'kde'"
+        self.interpolation = interpolation
         self.top = top
         self.conductor = conductor
         self.numlost = top.npslost[0]
@@ -16,7 +19,7 @@ class Conductor(object):
         self.cbounds = None
         self.x, self.y, self.z = None, None, None
         self.faces = []
-        self.pids = self.get_pids()
+        self.pids = self._get_pids()
 
         if w3d.solvergeom == w3d.XZgeom or w3d.solvergeom == w3d.RZgeom:
             self.axis = [0, 2, 0, 2]
@@ -27,18 +30,19 @@ class Conductor(object):
             self.domain = [w3d.xmmin, w3d.ymmin, w3d.zmmin, w3d.xmmax, w3d.ymmax, w3d.zmmax]
             self.cell_size = [w3d.dx, w3d.dy, w3d.dz]
 
-    def get_pids(self):
+    def _get_pids(self):
         scraped_particles = np.where(self.top.pidlost[:self.numlost, -1] == self.conductor.condid)[0]
 
         return scraped_particles
 
-    def _generate_face(self, resolution=100):
+    def _generate_face(self):
         scraped_parts = np.array([self.top.xplost, self.top.yplost, self.top.zplost]).T[self.pids, :]
         for bound, axis, face in zip(self.cbounds, self.axis, self.faces):
             xp = 1 + 5 * np.abs(self.cbounds[(axis + 1) % 3] - self.cbounds[(axis + 1) % 3 + 3]) \
-                 / self.cell_size[(axis + 1) % 3]
+                // self.cell_size[(axis + 1) % 3]
             yp = 1 + 5 * np.abs(self.cbounds[(axis + 2) % 3] - self.cbounds[(axis + 2) % 3 + 3]) \
-                 / self.cell_size[(axis + 2) % 3]
+                // self.cell_size[(axis + 2) % 3]
+
             x, y = np.meshgrid(np.linspace(self.cbounds[(axis + 1) % 3],
                                            self.cbounds[(axis + 1) % 3 + 3], xp),
                                np.linspace(self.cbounds[(axis + 2) % 3],
@@ -47,9 +51,18 @@ class Conductor(object):
             positions = np.vstack([x.ravel(), y.ravel()])
 
             if face.size > 30:
-                # 30 is empirically chosen as a bound where the KDE returns reasonable results
-                kernel = gaussian_kde(scraped_parts[face, :][:, [(axis + 1) % 3, (axis + 2) % 3]].T)
-                s = np.reshape(kernel(positions).T, x.shape)
+                if self.interpolation == 'kde':
+                    # 30 is empirically chosen as a bound where the KDE returns reasonable results
+                    kernel = gaussian_kde(scraped_parts[face, :][:, [(axis + 1) % 3, (axis + 2) % 3]].T)
+                    # Use surface point number as part of the normalization to prevent small surfaces returning outsized
+                    # results from the KDE
+                    s = np.reshape(kernel(positions).T, x.shape) * x.size
+                elif self.interpolation == 'cubic':
+                    s, _, _ = np.histogram2d(scraped_parts[face, (axis + 1) % 3], scraped_parts[face, (axis + 2) % 3],
+                                             bins=x.shape, range=[[self.cbounds[(axis + 1) % 3],
+                                               self.cbounds[(axis + 1) % 3 + 3]], [self.cbounds[(axis + 2) % 3],
+                                               self.cbounds[(axis + 2) % 3 + 3]]])
+                    s = griddata(positions.T, s.ravel(), (x, y), method='cubic') * x.size
             elif face.size > 1:
                 s, _, _ = np.histogram2d(scraped_parts[face, (axis + 1) % 3], scraped_parts[face, (axis + 2) % 3],
                                          bins=x.shape)
@@ -60,8 +73,8 @@ class Conductor(object):
 
 
 class BoxPlot(Conductor):
-    def __init__(self, top, w3d, conductor):
-        super(BoxPlot, self).__init__(top, w3d, conductor)
+    def __init__(self, top, w3d, conductor, interpolation='kde'):
+        super(BoxPlot, self).__init__(top, w3d, conductor, interpolation=interpolation)
         self.center = conductor.xcent, conductor.ycent, conductor.zcent
         self.size = conductor.xsize, conductor.ysize, conductor.zsize
 
@@ -104,24 +117,24 @@ class Plane(Conductor):
 
 
 class XPlanePlot(Plane):
-    def __init__(self, top, w3d, conductor):
-        super(XPlanePlot, self).__init__(top, w3d, conductor)
+    def __init__(self, top, w3d, conductor, interpolation='kde'):
+        super(XPlanePlot, self).__init__(top, w3d, conductor, interpolation=interpolation)
         self.axis = [0, ]
         self.center = [conductor.xcent, ]
         self.cbounds = self.domain
 
 
 class YPlanePlot(Plane):
-    def __init__(self, top, w3d, conductor):
-        super(YPlanePlot, self).__init__(top, w3d, conductor)
+    def __init__(self, top, w3d, conductor, interpolation='kde'):
+        super(YPlanePlot, self).__init__(top, w3d, conductor, interpolation=interpolation)
         self.axis = [1, ]
         self.center = [conductor.ycent, ]
         self.cbounds = self.domain
 
 
 class ZPlanePlot(Plane):
-    def __init__(self, top, w3d, conductor):
-        super(ZPlanePlot, self).__init__(top, w3d, conductor)
+    def __init__(self, top, w3d, conductor, interpolation='kde'):
+        super(ZPlanePlot, self).__init__(top, w3d, conductor, interpolation=interpolation)
         self.axis = [2, ]
         self.center = [conductor.zcent, ]
         self.cbounds = self.domain
