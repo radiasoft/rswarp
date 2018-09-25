20b9886e9692eec959a4a1f7348e05d74abde888
Johan Carlsson <carlsson@radiasoft.net>
2018-09-25 21:12:02 +0000

    Now calls rsoopic functions to generate energy and angle random numbers

--- rswarp/ionization/crosssections.py
+++ rswarp/ionization/crosssections.py
@@ -14,7 +14,7 @@ Utility module for calculating ionization cross sections, a la:
 from __future__ import division
 import math
 import random
-import time
+#import time
 import numpy as np
 # any import from warp will trigger arg parsing, which will fail without this
 # in a notebook context (or anything else with non-warp commandline args)
@@ -22,18 +22,22 @@ import warpoptions
 warpoptions.ignoreUnknownArgs = True
 from warp import emass, clight, jperev
 from scipy.constants import physical_constants, fine_structure # fine-structure constant
+import rsoopic.h2crosssections as h2crosssections
 
 class IonizationTarget:
     """
-    Base class for all ionization targets. Uses the Moller cross section,
-    modified to avoid zero-incident-energy singularity (Eq. 16 in Ref. [1]).
-    The getCrossSection method is intended to be customized for specific target
-    atom/molecule/ion.
+    Base class for all ionization targets. Uses the Moller cross section
+    (Eq. 2.15 in Ref. [2]), which has a free parameter eps_min.
+    The getCrossSection method is intended to be customized for specific
+    target atom/molecule/ion.
     """
-    a_0 = physical_constants['Bohr radius'][0]
     emassEV = emass * clight**2 / jperev
     useMollerApproximation = True
-    eps_min = 10. # value for which Kim and Lehtinen versions of Moller agree
+    # Value of eps_min for which Kim and Lehtinen versions of Moller agree for H2
+    eps_min = 8. # (in eV)
+
+    def setEps_min(self, val):
+        self.eps_min = val
 
     def getCrossSection(self, vi):
         """
@@ -41,175 +45,65 @@ class IonizationTarget:
         (in units of m**2)
         vi - incident electron velocity (in units of m/s, this is passed in from
         warp as vxi = uxi * gaminvi)
-        Cross section sigma is given by Eq. 16 in Ref. [1]
+        Cross section sigma is given by Eq. 2.15 in Ref. [2]
         """
         # initialize needed variables
+        r_0 = physical_constants['classical electron radius'][0]
+        mec2 = emass * clight**2
         beta_t = vi / clight
-        bprime = 0. # Moller should mean negligible binding energy
-        tprime = 1. / math.sqrt(1. - beta_t**2) - 1.
-        sigma = 1. - 1. / t - math.log(t) / (t + 1.) * (1. + 2. * tprime) / (1. + tprime)**2
-        sigma += bprime**2 / (1. + tprime)**2 * (t - 1.) / 2.
-        sigma *= 4. * np.pi * self.a_0**2 * fine_structure**2 * N * (R / I) / (beta_t * beta_t)
+        kappa = 2. * math.pi * r_0**2 * mec2 / (beta_t * beta_t)
+        eps_min_joule = self.eps_min * jperev
+        eps = mec2 * (1. / math.sqrt(1. - beta_t**2) - 1.) # incident energy (in J)
+        # now add terms to sigma, one by one:
+        sigma = 1. / eps_min_joule - 1. / (eps - eps_min_joule)
+        sigma += (eps - 2. * eps_min_joule) / (2. * (mec2 + eps)**2)
+        sigma += mec2 * (mec2 + 2. * eps) / (eps * (mec2 + eps)**2) * \
+        math.log(eps_min_joule / (eps - eps_min_joule))
+        sigma *= kappa
 
         return sigma
 
     def ejectedEnergy(self, vi, nnew):
         """
-        Selection of an ejected electron energy (in eV), adapted from
-        XOOPIC's MCCPackage::ejectedEnergy routine
+        Use Moller approximation, or delegate calculation to rsoopic (return
+        result from call to rsoopic function h2crosssections.ejectedEnergy)
         """
-        vi = vi[0:nnew]  # We may be given more velocities than we actually need
-
-        tstart = time.time()
-        gamma_inc = 1./np.sqrt(1.-(vi/clight)**2)
-        impactEnergy = (gamma_inc-1.) * self.emassEV
-
         if self.useMollerApproximation:
-            wOutMoller = np.empty((nnew))
+            vi = vi[0:nnew]  # We may be given more velocities than we actually need
+            gamma_inc = 1. / np.sqrt(1. - (vi / clight)**2)
+            impactEnergy = (gamma_inc - 1.) * self.emassEV
+            W = np.empty((nnew))
             for i in range(nnew):
                 Xrand = random.random()
-                wOutMoller[i] = impactEnergy[i] * self.eps_min / (impactEnergy[i] - Xrand * (impactEnergy[i] - 2 * self.eps_min))
-            return wOutMoller
-
-        tPlusMC = impactEnergy + self.emassEV
-        twoTplusMC = impactEnergy + tPlusMC
-        tPlusI = impactEnergy + self.I
-        tMinusI = impactEnergy - self.I
-
-        invTplusMCsq = 1. / (tPlusMC * tPlusMC)
-        tPlusIsq = tPlusI ** 2
-        iOverT = self.I / impactEnergy
-
-        funcT1 = 14./3. + .25 * tPlusIsq * invTplusMCsq - self.emassEV * twoTplusMC * iOverT * invTplusMCsq
-        funcT2 = 5./3. - iOverT - 2.*iOverT*iOverT/3. + .5 * self.I * tMinusI * invTplusMCsq + self.emassEV*twoTplusMC*self.I*invTplusMCsq*np.log(iOverT)/tPlusI
-
-        aGreaterThan = funcT1 * tMinusI / funcT2 / tPlusI
-
-        needToTryAgain = True
-        npart = nnew  # number of particles to generate on each loop
-        frand = np.random.uniform
-
-        wOut = np.array([])
-
-        while len(wOut) < nnew:
-            # print("%i particles left to generate for" % npart)
-
-            # wTest is the inverse of F(W)
-            # The random number, called F(W) in the notes and randomFW here,
-            #   is the antiderivative of a phony_but_simple probability, which
-            #   is always >= the correct_but_messy probability.
-
-            randomFW = frand(size=nnew) * aGreaterThan
-
-            # wTest is the inverse of F(W)
-            wTest = self.I*funcT2*randomFW/(funcT1-funcT2*randomFW)
-
-            # Because we are not working directly with the distribution function,
-            #   we must use the "rejection method".  This involves generating
-            #   another random number and seeing whether it is > the ratio of
-            #   the true probability over the phony_but_simple probability.
-
-            wPlusI       = wTest + self.I
-            wPlusIsq     = wPlusI * wPlusI
-            invTminusW   = 1./(impactEnergy-wTest)
-            invTminusWsq = invTminusW**2
-            invTminusW3  = invTminusW**3
-
-            probabilityRatio = (1. + 4.*self.I/wPlusI/3. + wPlusIsq*invTminusWsq + 4.*self.I*wPlusIsq*invTminusW3/3. - self.emassEV*twoTplusMC*wPlusI*invTminusW*invTplusMCsq + wPlusIsq*invTplusMCsq) / funcT1
-
-            mask = (probabilityRatio >= frand(size=nnew))
-            # npart -= np.sum(mask)  # Decrement by the number of passing particles
-
-            # Append the energies that meet the selection criterion
-            wOut = np.append(wOut, wTest[mask])
-
-        print("Spent %.3f s generating ejected energies" % (time.time()-tstart))
-        return wOut[0:nnew]  # Might possibly have more particles than necessary, but should have at least that many
+                W[i] = impactEnergy[i] * self.eps_min / \
+                (impactEnergy[i] - Xrand * (impactEnergy[i] - 2 * self.eps_min))
+            return W
+        else:
+            W = h2crosssections.ejectedEnergy(vi, nnew)
+            return W[0:nnew]
 
     def generateAngle(self, nnew, emitted_energy, incident_energy):
         """
-        emitted_energy - emitted electon energy (in eV)
-        incident_energy - incident electon energy (in eV)
-        nnew - number of new particles
-
-        Selection of after-ionization angles for the primary and secondary electrons
-        adapted from XOOPIC's MCCPackage::primarySecondaryAngles routine.
-
-        ## The general idea ##
-        The cross-section $\sigma(w, t, \theta)$ dictates the likelihood
-        of any given ionization event with incident energy $t$, emission energy $w$, and
-        emission angle $\theta$.  We will treat the recoiling primary as though it is
-        two separate particles, one before and one after ionization, with energy
-        reduced by the ionization energy and what is given to the secondary.
-
-        Note: theta is defined relative to the incident electron trajectory
-
-        We know that:
-            $ \int_0^\pi { \sigma(w, t, \theta) * 2\pi sin(\theta) d\theta } = \sigma(t,w) $
-
-        We can then define $F(\theta)$ taking the value $0 <= F(\theta) <= 1$:
-            $ F(\theta) = 2\pi \int_0^\theta d\theta' sin(\theta') \frac{\sigma(w, t, \theta')}{\sigma(w,t)} $
-
-        If we can invert this expression to get $\theta$ as a function of $w, t$, and $F(\theta)$, we can
-        sample this distribution by choosing a random number for $F(\theta)$ and the known values of $w, t$.
-
-        This inversion is explained in detail in [3].
+        Use Moller approximation, or delegate calculation to rsoopic (return
+        result from call to rsoopic function h2crosssections.generateAngle)
         """
-
         if self.useMollerApproximation:
-            thetaMoller = np.empty((nnew))
+            theta = np.empty((nnew))
             for i in range(nnew):
                 costheta = emitted_energy[i] * (incident_energy[i] + 2. * self.emassEV)
                 costheta /= incident_energy[i] * (emitted_energy[i] + 2. * self.emassEV)
                 costheta = math.sqrt(costheta)
-                thetaMoller[i] = math.acos(costheta)
-            return thetaMoller
-
-        T = incident_energy
-        W = emitted_energy
-        g2 = self.G_2(T, W)
-        g3 = self.G_3(T, W)
-        F = np.random.uniform(size=nnew)
-
-        theta = np.nan_to_num(np.arccos(g2 + g3 * np.tan(
-            (1-F) * np.arctan2((1-g2), g3) - F * np.arctan2((1+g2), g3))
-        ))
-        return theta
-
-    def alpha(self, E):
-        """
-        E - electron energy (in eV)
-        """
-        return 0.6 * (self.emassEV / (E + self.emassEV))**2
-
-    def G_2(self, T, W):
-        """
-        T - incident electron energy (in eV)
-        W - emitted electon energy (in eV)
-        """
-        return np.sqrt(np.divide(W+I, T) * np.divide(T+2*self.emassEV, W+2*self.emassEV))
-
-    def G_3(self, T, W):
-        """
-        T - incident electron energy (in eV)
-        W - emitted electon energy (in eV)
-        """
-        return self.alpha(T) * np.sqrt(np.divide(I, W) * np.divide(T - (W+I), T))
-
-    def normalizedKineticEnergy(self, vi=None):
-        """
-        Compute the normalized kinetic energy n = T/I given an input velocity
-        """
-        gamma_in = 1. / np.sqrt(1 - (vi/clight)**2)
-        T = (gamma_in - 1) * emass * clight**2 / jperev  # kinetic energy (in eV) of incident electron
-        t = T / self.I  # normalized kinetic energy
-        return t
+                theta[i] = math.acos(costheta)
+            return theta
+        else:
+            return h2crosssections.generateAngle(nnew, emitted_energy, incident_energy)
 
 class H2IonizationTarget(IonizationTarget):
     """
     Derived class using the RBEB cross section (Eq. 22 in Ref. [1]),
     with parameters set for H2 (hydrogen gas)
     """
+    a_0 = physical_constants['Bohr radius'][0]
     I = 15.42593 # threshold ionization energy (in eV), from the NIST Standard Reference Database (via NIST Chemistry WebBook)
     U = 15.98 # average orbital kinetic energy (in eV) (value taken from https://physics.nist.gov/PhysRefData/Ionization/intro.html)
     R = 13.60569 # Rydberg energy (in eV)
@@ -226,7 +120,7 @@ class H2IonizationTarget(IonizationTarget):
         warp as vxi = uxi * gaminvi)
         Cross section sigma is given by Eq. 22 in Ref. [1]
         """
-        t = self.normalizedKineticEnergy(vi)
+        t = h2crosssections.normalizedKineticEnergy(vi)
         if t <= 1:
             return 0.
 
@@ -244,37 +138,5 @@ class H2IonizationTarget(IonizationTarget):
         sigma += 1. - 1. / t - math.log(t) / (t + 1.) * (1. + 2. * tprime) / (1. + .5 * tprime)**2
         sigma += bprime**2 / (1. + .5 * tprime)**2 * (t - 1) / 2.
         sigma *= 4. * np.pi * self.a_0**2 * fine_structure**4 * self.N / (beta_t**2 + beta_u**2 + beta_b**2) / (2. * bprime)
-
-        return sigma
-
-class MollerIonizationTarget(IonizationTarget):
-    """
-    Derived class using alternative version of Moller cross section
-    (Eq. 2.15 in Ref. [2]), which has a free parameter eps_min
-    """
-    def setEps_min(self, val):
-        self.eps_min = val
-
-    def getCrossSection(self, vi):
-        """
-        Compute and return the total cross-section for electron impact ionization
-        (in units of m**2)
-        vi - incident electron velocity (in units of m/s, this is passed in from
-        warp as vxi = uxi * gaminvi)
-        Cross section sigma is given by Eq. 2.15 in Ref. [2]
-        """
-        # initialize needed variables
-        r_0 = physical_constants['classical electron radius'][0]
-        mec2 = emass * clight**2
-        beta_t = vi / clight
-        kappa = 2. * math.pi * r_0**2 * mec2 / (beta_t * beta_t)
-        eps_min = self.eps_min * jperev
-        eps = mec2 * (1. / math.sqrt(1. - beta_t**2) - 1.)
-        # now add terms to sigma, one by one:
-        sigma = 1. / eps_min - 1. / (eps - eps_min)
-        sigma += (eps - 2. * eps_min) / (2. * (mec2 + eps)**2)
-        sigma += mec2 * (mec2 + 2. * eps) / (eps * (mec2 + eps)**2) * \
-        math.log(eps_min / (eps - eps_min))
-        sigma *= kappa
 
         return sigma
