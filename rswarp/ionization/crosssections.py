74a67453cd47ad19c461020d8f9cec2c77c46ff5
Johan Carlsson <johan@uwalumni.com>
2018-10-04 18:04:09 -0400

    Added ion-impact ionization with cross section given by the Rayzer rule

--- rswarp/ionization/crosssections.py
+++ rswarp/ionization/crosssections.py
@@ -10,6 +10,14 @@ Utility module for calculating ionization cross sections, a la:
     Stanford University PhD Thesis, 2000
     <http://nova.stanford.edu/~vlf/publications/theses/nlehtthesis.pdf>
 [3] D. Bruhwiler, "RNG Calculations for Scattering in XOOPIC", Tech-X Note, 2000
+[4] M. E. Rudd, Y.-K. Kim, D. H. Madison and J. W. Gallagher,
+    "Electron production in proton collisions: total cross sections",
+    Reviews of Modern Physics, Vol. 57, No. 4, October 1985
+    <https://digitalcommons.unl.edu/cgi/viewcontent.cgi?article=1041&context=physicsrudd>
+[5] Ya. B. Zeldovich and Yu. P. Rayzer,
+    "Physics of shock waves and high-temperature hydrodynamic phenomena",
+    FTD-MT 64-514, 30 September 1965
+    <http://www.dtic.mil/dtic/tr/fulltext/u2/622356.pdf>
 """
 from __future__ import division
 import math
@@ -24,17 +32,43 @@ from warp import emass, clight, jperev
 from scipy.constants import physical_constants, fine_structure # fine-structure constant
 import rsoopic.h2crosssections as h2crosssections
 
-class IonizationTarget:
+echarge = jperev
+pmass = 1.8362e3 * emass
+H2mass = 1.66e-27 # mass of hydrogen molecule (in kg)
+
+class IonizationEvent:
     """
-    Base class for all ionization targets. Uses the Moller cross section
-    (Eq. 2.15 in Ref. [2]), which has a free parameter eps_min.
+    Base class for all ionization events. Uses the Moller cross section
+    for electron-impact ionization (Eq. 2.15 in Ref. [2]), which has a
+    free parameter eps_min.
     The getCrossSection method is intended to be customized for specific
     target atom/molecule/ion.
     """
+    R = 13.60569 # Rydberg energy (in eV)
+    a_0 = physical_constants['Bohr radius'][0]
+
     emassEV = emass * clight**2 / jperev
+
     useMollerApproximation = True
-    # Value of eps_min for which Kim and Lehtinen versions of Moller agree for H2
+
+    # Value of eps_min for which Kim [1] and Lehtinen [2] versions
+    # of Moller agree for H2
     eps_min = 8. # (in eV)
+
+    class Target: # container for target-particle parameters
+        def __init__(self, number_of_electrons, mass, charge):
+            self.N = number_of_electrons
+            if mass is not None: self.mass = mass
+            if charge != 0.: self.charge = charge
+
+    class Projectile: # container for projectile-particle parameters
+        def __init__(self, mass, charge):
+            self.mass = mass
+            self.charge = charge
+
+    def __init__(self, trgN = 1, trgm = None, trgq = 0.):
+        self.target = Target(trgN, trgm, trgq)
+        self.projectile = Projectile(emass, echarge)
 
     def setEps_min(self, val):
         self.eps_min = val
@@ -60,6 +94,7 @@ class IonizationTarget:
         sigma += mec2 * (mec2 + 2. * eps) / (eps * (mec2 + eps)**2) * \
         math.log(eps_min_joule / (eps - eps_min_joule))
         sigma *= kappa
+        sigma *= self.target.N
 
         return sigma
 
@@ -98,19 +133,17 @@ class IonizationTarget:
         else:
             return h2crosssections.generateAngle(nnew, emitted_energy, incident_energy)
 
-class H2IonizationTarget(IonizationTarget):
+class H2IonizationEvent(IonizationEvent):
     """
     Derived class using the RBEB cross section (Eq. 22 in Ref. [1]),
     with parameters set for H2 (hydrogen gas)
     """
-    a_0 = physical_constants['Bohr radius'][0]
-    I = 15.42593 # threshold ionization energy (in eV), from the NIST Standard Reference Database (via NIST Chemistry WebBook)
-    U = 15.98 # average orbital kinetic energy (in eV) (value taken from https://physics.nist.gov/PhysRefData/Ionization/intro.html)
-    R = 13.60569 # Rydberg energy (in eV)
-    N = 2 # number of electrons in target (H2)
-
     def __init__(self):
+        super(H2IonizationEvent, self).__init__(self)
         self.useMollerApproximation = False
+        self.target.N = 2 # number of electrons in target (H2)
+        self.target.I = 15.42593 # threshold ionization energy (in eV), from the NIST Standard Reference Database (via NIST Chemistry WebBook)
+        self.target.U = 15.98 # average orbital kinetic energy (in eV), value taken from https://physics.nist.gov/PhysRefData/Ionization/intro.html
 
     def getCrossSection(self, vi):
         """
@@ -128,15 +161,53 @@ class H2IonizationTarget(IonizationTarget):
 
         # initialize needed variables
         beta_t = vi / clight
-        beta_u = beta(self.U)
-        beta_b = beta(self.I)
-        bprime = self.I / self.emassEV
+        beta_u = beta(self.target.U)
+        beta_b = beta(self.target.I)
+        bprime = self.target.I / self.emassEV
         tprime = t * bprime
         # now add terms to sigma, one by one:
         sigma = math.log(beta_t**2 / (1. - beta_t**2)) - beta_t**2 - math.log(2. * bprime)
         sigma *= .5 * (1. - 1. / (t * t))
         sigma += 1. - 1. / t - math.log(t) / (t + 1.) * (1. + 2. * tprime) / (1. + .5 * tprime)**2
         sigma += bprime**2 / (1. + .5 * tprime)**2 * (t - 1) / 2.
-        sigma *= 4. * np.pi * self.a_0**2 * fine_structure**4 * self.N / (beta_t**2 + beta_u**2 + beta_b**2) / (2. * bprime)
+        sigma *= 4. * np.pi * self.a_0**2 * fine_structure**4 * self.target.N / (beta_t**2 + beta_u**2 + beta_b**2) / (2. * bprime)
 
         return sigma
+
+class IonIonizationEvent(IonizationEvent):
+    """
+    Base class for ion-impact ionization events. Uses the "Rayzer rule"
+    (p. 514 in Ref. [5], and also last paragraph of section III.B in Ref. [4])
+    to transform electron-impact ionization cross section into corresponding
+    cross section for any incident ion.
+    """
+    def __init__(self, pm = pmass, pq = echarge, trgN = 1, trgm = None, trgq = 0.):
+        super(IonIonizationEvent, self).__init__(self)
+        self.projectile.mass = pm
+        self.projectile.charge = pq
+        target.self.N = trgN
+        if trgm is not None: target.self.mass = trgm
+        if trgq != 0.: target.self.charge = trgq
+
+    def getCrossSection(self, vi):
+        """
+        Transform incident velocity vi using Rayzer rule, get electron
+        cross section from super class, and scale it for incident ion charge
+        """
+        # Scale up ion velocity to that of electron with same kinetic energy
+        vi *= math.sqrt(self.projectile.mass / emass)
+        # Get electron-impact ionization cross section from super-class method
+        sigma = super(IonIonizationEvent, self).getCrossSection(vi)
+        # Scale cross section with charge
+        sigma *= (self.projectile.charge / echarge)**2
+
+        return sigma
+
+    def ejectedEnergy(self, vi, nnew):
+        """
+        Transform incident velocity vi using Rayzer rule, then delegate to
+        super-class method
+        """
+        # Scale up ion velocity to that of electron with same kinetic energy
+        vi *= math.sqrt(self.projectile.mass / emass)
+        return super(IonIonizationEvent, self).ejectedEnergy(vi, nnew)
