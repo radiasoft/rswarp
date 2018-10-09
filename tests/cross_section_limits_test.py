870f03a86d75c63ec00bb417e95636684dac65b1
Johan Carlsson <johan@uwalumni.com>
2018-10-09 13:41:30 -0400

    Added unit tests for ion-impact ionization

--- tests/cross_section_limits_test.py
+++ tests/cross_section_limits_test.py
@@ -3,7 +3,7 @@ import math
 import sys
 
 import rsoopic.h2crosssections as h2crosssections
-from scipy.constants import c, e, m_e
+from scipy.constants import c, e, m_e, m_p
 
 def test_cross_section_limits():
     """
@@ -45,6 +45,26 @@ def test_cross_section_limits():
     if __name__ == '__main__':
         print T, sigma
     assert(2.3e-23 < sigma and 2.5e-23 > sigma)
+
+    gamma = e * T / (m_p * c**2) + 1. # gamma value for incident proton
+    #v_T = c * np.sqrt(1. - np.divide(1., gamma**2))
+    v_T = c * math.sqrt(1. - 1. / gamma**2)
+
+    # Calculate cross section using Rayzer rule
+    p_on_h2 = Xsect.IonIonizationEvent()
+    sigma = p_on_h2.getCrossSection(v_T)
+    if __name__ == '__main__':
+        print T, sigma
+    assert(1.3e-21 < sigma and 1.5e-21 > sigma)
+
+    # Calculate cross section using Rudd model
+    p_on_h2 = Xsect.RuddIonIonizationEvent()
+    sigma = p_on_h2.getCrossSection(v_T)
+    if __name__ == '__main__':
+        print T, sigma
+    assert(3.65e-21 < sigma and 3.85e-21 > sigma)
+
+    #print p_on_h2.eps_min
 
 if __name__ == '__main__':
     test_cross_section_limits()
