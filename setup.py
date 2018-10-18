33850233e24221c51124efcb98b1ec88f8c61e57
chall <chall@radiasoft.net>
2018-10-18 16:23:48 -0600

    Fixing setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ u"""rswarp setup script
 :copyright: Copyright (c) 2016 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
-from pykern.pksetup import setup
+from setuptools import setup
 
 setup(
     name='rswarp',
