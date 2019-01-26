a1f03e1549b8da6f79e70b04dbc5e00109d50965
Johan Carlsson <johan@uwalumni.com>
2019-01-26 00:43:22 -0500

    Added calculation of charge neutralization efficiency. Now puts density graphs for all species in same plot.

--- examples/ionization/density_plot.py
+++ examples/ionization/density_plot.py
@@ -6,9 +6,9 @@ import numpy as np
 import math
 import sys
 
-Nt = 2000000
+Nt = 1000000
 Nr = 128
-Nz = 128
+Nz = 64
 
 pipe_radius = 0.1524 / 2. * 100.
 pipe_length = 200.
@@ -17,12 +17,14 @@ dr = pipe_radius / Nr
 dz = pipe_length / Nz
 
 dens = np.empty((Nr, Nz))
+tot_e_dens_r = np.empty((Nr))
+tot_e_dens_z = np.empty((Nz))
 
-density_plot, ax = plt.subplots(3, 2, figsize=(12, 12))
+density_plot, ax = plt.subplots(1, 2, figsize=(12, 6))
 # exlicitly make density_plot the active figure:
 plt.figure(density_plot.number)
-plt.suptitle('Particle densitites after ' + str(Nt) + ' time steps')
-plt.subplots_adjust(hspace=0.4)
+plt.suptitle('Particle densitites after ' + str(Nt) + ' time steps with drain electrodes at +20 V')
+#plt.subplots_adjust(hspace=0.4)
 
 f = h5py.File('diags/hdf5/data0' + str(Nt) + '.h5', 'r')
 
@@ -55,18 +57,38 @@ for n in range(3):
     dens[:, :] *= 1.e-6
     dens[0, 0] = 0.
 
-    ax[n, 0].set_title(species[n])
-    ax[n, 0].set_xlabel('radial location (cm)')
-    ax[n, 0].set_ylabel('density (10^6/cm^3)')
-    ax[n, 0].set_xlim(0., pipe_radius)
-    ax[n, 0].plot(np.linspace(0., pipe_radius, Nr), dens[:, Nz / 2])
+    #ax[0].set_title(species[n])
+    ax[0].set_xlabel('radial location (cm)')
+    ax[0].set_ylabel('density (10^6/cm^3)')
+    ax[0].set_xlim(0., pipe_radius)
+    ax[0].plot(np.linspace(0., pipe_radius, Nr), dens[:, Nz / 2], label = species[n])
 
-    ax[n, 1].set_title(species[n])
-    ax[n, 1].set_xlabel('axial location (cm)')
-    ax[n, 1].set_ylabel('density (10^6/cm^3)')
-    ax[n, 1].set_xlim(0., pipe_length)
-    ax[n, 1].plot(np.linspace(0., pipe_length, Nz), dens[0, :])
+    if n == 0:
+        tot_e_dens_r[:] = dens[:, Nz / 2]
+        neb = np.sum(tot_e_dens_r)
+    if n == 1:
+        tot_e_dens_r[:] += dens[:, Nz / 2]
+        net = np.sum(tot_e_dens_r)
+        ax[0].plot(np.linspace(0., pipe_radius, Nr), tot_e_dens_r, label = 'electrons total')
+    if n == 2:
+        ni = np.sum(dens[:, Nz / 2])
+
+    #ax[1].set_title(species[n])
+    ax[1].set_xlabel('axial location (cm)')
+    ax[1].set_ylabel('density (10^6/cm^3)')
+    ax[1].set_xlim(0., pipe_length)
+    ax[1].plot(np.linspace(0., pipe_length, Nz), dens[0, :], label = species[n])
+
+    if n == 0:
+        tot_e_dens_z[:] = dens[0, :]
+    if n == 1:
+        tot_e_dens_z[:] += dens[0, :]
+        ax[1].plot(np.linspace(0., pipe_length, Nz), tot_e_dens_z, label = 'electrons total')
 
 #density_plot.tight_layout()
+ax[0].legend(prop = {'size': 10}, loc = 'upper right')
+ax[1].legend(prop = {'size': 10}, loc = 'upper left')
 density_plot.savefig('density_plot-' + str(Nt) + '.png')
 plt.close()
+
+print 'charge neutralization efficiency is ', 100. * (1. - (net - ni) / neb), '%'
