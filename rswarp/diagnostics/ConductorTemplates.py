ff48b239bdaed796cdf6d95156ab9e9caa3d0259
chall <chall@radiasoft.net>
2019-02-11 16:51:56 -0700

    Refactored 3D plotting routine to remove mesh creation into each type of conductor indvidually

--- rswarp/diagnostics/ConductorTemplates.py
+++ rswarp/diagnostics/ConductorTemplates.py
@@ -1,5 +1,5 @@
 import numpy as np
-from warp.field_solvers.generateconductors import XPlane, YPlane, ZPlane, Box
+from warp.field_solvers.generateconductors import XPlane, YPlane, ZPlane, Box, Sphere
 from scipy.stats import gaussian_kde
 from scipy.interpolate import griddata
 
@@ -20,6 +20,7 @@ class Conductor(object):
         self.x, self.y, self.z = None, None, None
         self.faces = []
         self.pids = self._get_pids()
+        self.thresshold = 30  # how many particles need to be lost to generate a colormap
 
         if w3d.solvergeom == w3d.XZgeom or w3d.solvergeom == w3d.RZgeom:
             self.axis = [0, 2, 0, 2]
@@ -35,41 +36,24 @@ class Conductor(object):
 
         return scraped_particles
 
-    def _generate_face(self):
-        scraped_parts = np.array([self.top.xplost, self.top.yplost, self.top.zplost]).T[self.pids, :]
-        for bound, axis, face in zip(self.cbounds, self.axis, self.faces):
-            xp = 1 + 5 * np.abs(self.cbounds[(axis + 1) % 3] - self.cbounds[(axis + 1) % 3 + 3]) \
-                // self.cell_size[(axis + 1) % 3]
-            yp = 1 + 5 * np.abs(self.cbounds[(axis + 2) % 3] - self.cbounds[(axis + 2) % 3 + 3]) \
-                // self.cell_size[(axis + 2) % 3]
+    def _color_mesh(self, mesh, particle_subset=None):
+        # all particles lost on conductor
+        if particle_subset is not None:
+            pids = particle_subset
+        else:
+            pids = self.pids
+        scraped_parts = np.array([self.top.xplost, self.top.yplost, self.top.zplost])[:, pids]
 
-            x, y = np.meshgrid(np.linspace(self.cbounds[(axis + 1) % 3],
-                                           self.cbounds[(axis + 1) % 3 + 3], xp),
-                               np.linspace(self.cbounds[(axis + 2) % 3],
-                                           self.cbounds[(axis + 2) % 3 + 3], yp))
-            z = np.ones_like(x) * bound
-            positions = np.vstack([x.ravel(), y.ravel()])
+        if scraped_parts.shape[1] > self.thresshold:
+            if self.interpolation == 'kde':
+                kernel = gaussian_kde(scraped_parts)
+                # Use surface point number as part of the normalization to prevent small surfaces returning outsized
+                # results from the KDE
+                s = kernel(mesh).T  # * mesh.size
+        else:
+            s = np.ones_like(mesh[0, :]) * -1.0
 
-            if face.size > 30:
-                if self.interpolation == 'kde':
-                    # 30 is empirically chosen as a bound where the KDE returns reasonable results
-                    kernel = gaussian_kde(scraped_parts[face, :][:, [(axis + 1) % 3, (axis + 2) % 3]].T)
-                    # Use surface point number as part of the normalization to prevent small surfaces returning outsized
-                    # results from the KDE
-                    s = np.reshape(kernel(positions).T, x.shape) * x.size
-                elif self.interpolation == 'cubic':
-                    s, _, _ = np.histogram2d(scraped_parts[face, (axis + 1) % 3], scraped_parts[face, (axis + 2) % 3],
-                                             bins=x.shape, range=[[self.cbounds[(axis + 1) % 3],
-                                               self.cbounds[(axis + 1) % 3 + 3]], [self.cbounds[(axis + 2) % 3],
-                                               self.cbounds[(axis + 2) % 3 + 3]]])
-                    s = griddata(positions.T, s.ravel(), (x, y), method='cubic') * x.size
-            elif face.size > 1:
-                s, _, _ = np.histogram2d(scraped_parts[face, (axis + 1) % 3], scraped_parts[face, (axis + 2) % 3],
-                                         bins=x.shape)
-            else:
-                s = np.ones_like(x) * -1.0
-
-            yield x, y, z, s
+        return s
 
 
 class BoxPlot(Conductor):
@@ -88,31 +72,61 @@ class BoxPlot(Conductor):
         scraped_parts = np.array([self.top.xplost, self.top.yplost, self.top.zplost]).T[self.pids, :]
         for bound, axis, cell_size in zip(self.cbounds, self.axis, 2 * self.cell_size):
             pof = np.where(np.abs(scraped_parts[:, axis] - bound) <= cell_size)
-            self.faces.append(pof[0])
+            yield pof[0]
 
     def generate_faces(self):
-        if len(self.faces) == 0:
-            self.get_particles()
-        for axis, mesh in zip(self.axis, self._generate_face()):
-            x, y, z = [mesh[0], mesh[1], mesh[2]][(2 * axis + 2) % 3], \
-                      [mesh[0], mesh[1], mesh[2]][(2 * axis + 3) % 3], \
-                      [mesh[0], mesh[1], mesh[2]][(2 * axis + 1) % 3]
-            s = mesh[3]
+
+        for bound, axis, pids in zip(self.cbounds, self.axis, self.get_particles()):
+
+
+            xn = 1 + 5 * np.abs(self.cbounds[(axis + 1) % 3] - self.cbounds[(axis + 1) % 3 + 3]) \
+                // self.cell_size[(axis + 1) % 3]
+            yn = 1 + 5 * np.abs(self.cbounds[(axis + 2) % 3] - self.cbounds[(axis + 2) % 3 + 3]) \
+                // self.cell_size[(axis + 2) % 3]
+
+            x, y = np.meshgrid(np.linspace(self.cbounds[(axis + 1) % 3],
+                                           self.cbounds[(axis + 1) % 3 + 3], xn),
+                               np.linspace(self.cbounds[(axis + 2) % 3],
+                                           self.cbounds[(axis + 2) % 3 + 3], yn))
+            z = np.ones_like(x) * bound
+
+            x, y, z = [x, y, z][(2 * axis + 2) % 3], \
+                      [x, y, z][(2 * axis + 3) % 3], \
+                      [x, y, z][(2 * axis + 1) % 3]
+            print("BOX:")
+            print("Bounds x:", np.min(x), np.max(x))
+            print("Bounds y:", np.min(y), np.max(y))
+            print("Bounds z:", np.min(z), np.max(z))
+            print("Particle count:", pids.size)
+            print()
+            s = self._color_mesh(mesh=np.vstack([x.ravel(), y.ravel(), z.ravel()]), particle_subset=pids).reshape(x.shape)
+
             yield x, y, z, s
 
 
 class Plane(Conductor):
 
-    def get_particles(self):
-        # The plane has only one face so we use all particles with appropriate pid
-        self.faces.append(np.arange(self.pids.size))
-
     def generate_faces(self):
-        if len(self.faces) == 0:
-            self.get_particles()
-        for mesh in self._generate_face():
-            x, y, z = mesh[0], mesh[1], np.ones_like(mesh[0]) * self.center[0]
-            s = mesh[3]
+        for bound, axis in zip(self.cbounds, self.axis):
+            xn = 1 + 5 * np.abs(self.cbounds[(axis + 1) % 3] - self.cbounds[(axis + 1) % 3 + 3]) \
+                 // self.cell_size[(axis + 1) % 3]
+            yn = 1 + 5 * np.abs(self.cbounds[(axis + 2) % 3] - self.cbounds[(axis + 2) % 3 + 3]) \
+                 // self.cell_size[(axis + 2) % 3]
+
+            x, y = np.meshgrid(np.linspace(self.cbounds[(axis + 1) % 3],
+                                           self.cbounds[(axis + 1) % 3 + 3], xn),
+                               np.linspace(self.cbounds[(axis + 2) % 3],
+                                           self.cbounds[(axis + 2) % 3 + 3], yn))
+            z = np.ones_like(x) * self.center[0]
+            s = self._color_mesh(mesh=np.vstack([x.ravel(), y.ravel(), z.ravel()]), particle_subset=None).reshape(x.shape)
+
+            print("PLANE:")
+            print("Bounds x:", np.min(x), np.max(x))
+            print("Bounds y:", np.min(y), np.max(y))
+            print("Bounds z:", np.min(z), np.max(z))
+            print("Particle count:", self.pids.size)
+            print()
+
             yield x, y, z, s
 
 
@@ -140,7 +154,32 @@ class ZPlanePlot(Plane):
         self.cbounds = self.domain
 
 
+class SpherePlot(Conductor):
+    def __init__(self, top, w3d, conductor, interpolation='kde'):
+        super(SpherePlot, self).__init__(top, w3d, conductor, interpolation=interpolation)
+        self.center = [conductor.xcent, conductor.ycent, conductor.zcent]
+        self.radius = conductor.radius
+
+    def generate_faces(self):
+        # TODO: Sphere has a fixed number of points right now. Everything else scales with the solver mesh spacing.
+        print("SHERE HERE", self.pids.size)
+        dphi = np.linspace(0, 2 * np.pi, 250)
+        dtheta = np.linspace(-np.pi, np.pi, 250)
+        phi, theta = np.meshgrid(dphi, dtheta)
+        x = self.radius * np.cos(phi) * np.sin(theta) + self.center[0]
+        y = self.radius * np.sin(phi) * np.sin(theta) + self.center[1]
+        z = self.radius * np.cos(theta) + self.center[2]
+
+        s = self._color_mesh(mesh=np.vstack([x.ravel(), y.ravel(), z.ravel()]), particle_subset=None).reshape(x.shape)
+
+        yield x, y, z, s
+
+
+
+
+
 conductor_type = {XPlane: XPlanePlot,
                   YPlane: YPlanePlot,
                   ZPlane: ZPlanePlot,
-                  Box: BoxPlot}
+                  Box: BoxPlot,
+                  Sphere: SpherePlot}
