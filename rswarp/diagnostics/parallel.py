57292885aa0d8af5d6ec928eb001defd4ac7b1dd
chall <chall@radiasoft.net>
2019-08-14 11:57:42 -0600

    Split lost array communication out from save function

--- rswarp/diagnostics/parallel.py
+++ rswarp/diagnostics/parallel.py
@@ -1,8 +1,60 @@
 import numpy as np
 import h5py as h5
+from warp import toperror
 """
 Utilities for parallel Warp operation.
 """
+
+
+def gather_lost_particles(top, comm_world):
+
+    xplost_all = []
+    yplost_all = []
+    zplost_all = []
+
+    # Check that all arrays needed to process locations exist on each rank
+    try:
+        top.xplost
+        top.yplost
+        top.zplost
+        status = 1
+    except toperror:
+        status = 0
+
+    # Send each status to head
+    all_status = comm_world.gather(status, root=0)
+
+    # Prepare head to receive particles arrays from all valid ranks
+    if comm_world.rank == 0:
+        for rank, status in enumerate(all_status):
+            if status == 1 and rank != 0:
+                xplost_all.append(comm_world.recv(source=rank, tag=0))
+                yplost_all.append(comm_world.recv(source=rank, tag=1))
+                zplost_all.append(comm_world.recv(source=rank, tag=2))
+
+    # All ranks that have valid data send to head
+    if status == 1 and comm_world.rank != 0:
+        comm_world.send(top.xplost, dest=0, tag=0)
+        comm_world.send(top.yplost, dest=0, tag=1)
+        comm_world.send(top.zplost, dest=0, tag=2)
+
+    # Process data on head
+    if comm_world.rank == 0:
+        # If the head has particle data then add it to array
+        if all_status[0] == 1:
+            xplost_all = [top.xplost] + xplost_all
+            yplost_all = [top.yplost] + yplost_all
+            zplost_all = [top.zplost] + zplost_all
+        # If any particle data exists then concatenate all of it together and save
+        if len(xplost_all) > 0:
+            xplost_all = np.hstack(xplost_all)
+            yplost_all = np.hstack(yplost_all)
+            zplost_all = np.hstack(zplost_all)
+
+    if comm_world.rank == 0:
+        return xplost_all, yplost_all, zplost_all
+    else:
+        return None, None, None
 
 
 def save_lost_particles(top, comm_world, fsave=None):
@@ -23,50 +75,11 @@ def save_lost_particles(top, comm_world, fsave=None):
     if not fsave:
         fsave = 'lost_particles_step_{}.npy'.format(top.it)
 
-    xplost_all = []
-    yplost_all = []
-    zplost_all = []
-
-    # Check that all arrays needed to process locations exist on each rank
-    try:
-        top.xplost
-        top.yplost
-        top.zplost
-        status = 1
-    except:
-        status = 0
-        "Rank {} does not hold lost particle arrays".format(comm_world.rank)
-
-    # Send each status to head
-    all_status = comm_world.gather(status, root=0)
-
-    # Prepare head to receive particles arrays from all valid ranks
-    if comm_world.rank == 0:
-        for rank, status in enumerate(all_status):
-            if status == 1 and rank != 0:
-                xplost_all.append(comm_world.recv(source=rank, tag=0))
-                yplost_all.append(comm_world.recv(source=rank, tag=1))
-                zplost_all.append(comm_world.recv(source=rank, tag=2))
-
-    # All ranks that have valid data send to head
-    if status == 1 and comm_world.rank != 0:
-        comm_world.send(top.xplost, dest=0, tag=0)
-        comm_world.send(top.yplost, dest=0, tag=1)
-        comm_world.send(top.zplost, dest=0, tag=2)
+    xplost_all, yplost_all, zplost_all = gather_lost_particles(top, comm_world)
 
     # Process data on head and save
     if comm_world.rank == 0:
-        # If the head has particle data then add it to array
-        if all_status[0] == 1:
-            xplost_all = [top.xplost] + xplost_all
-            yplost_all = [top.yplost] + yplost_all
-            zplost_all = [top.zplost] + zplost_all
-        # If any particle data exists then concatenate all of it together and save
-        if len(xplost_all) > 0:
-            xplost_all = np.hstack(xplost_all)
-            yplost_all = np.hstack(yplost_all)
-            zplost_all = np.hstack(zplost_all)
-            np.save(fsave, [xplost_all, yplost_all, zplost_all])
+        np.save(fsave, [xplost_all, yplost_all, zplost_all])
 
 
 def save_pidlost(top, comm_world, species, fsave=None):
